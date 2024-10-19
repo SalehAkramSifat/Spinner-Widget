df372660aec65fe88dc30c3586d2bb5171efd9a1
Saleh Akram Sifat <salehakramsifat@gmail.com>
2024-10-19 22:14:03 +0600

    Initial Commit

--- app/src/main/java/com/sifat/spinnerwidget/MainActivity.kt
+++ app/src/main/java/com/sifat/spinnerwidget/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.sifat.spinnerwidget
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.sifat.spinnerwidget.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        val listOfLanguages = listOf("Bangla", "English", "Hindi", "Arabic", "Spanish")
+
+        val arrayAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listOfLanguages)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinner.adapter = arrayAdapter
+
+        binding.spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                val selectedItem = parent?.getItemAtPosition(position).toString()
+                Toast.makeText(this@MainActivity, "You have selected the $selectedItem language", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+        }
     }
 }
